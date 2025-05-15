f399beaf1bd099e1089cf2e21a0cb7faaf1e0679
gowthamr1312 <157381691+gowthamr1312@users.noreply.github.com>
2025-05-15 08:38:16 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,53 +1,57 @@
-const API_ENDPOINT = "https://71gaj43a5l.execute-api.ap-south-1.amazonaws.com/prod";
-const LOG_ENDPOINT = "https://zcrjgg8oag.execute-api.ap-south-1.amazonaws.com/Dev/";
+<script>
+  // Visitor Tracking (GET)
+  var TRACKING_API_URL = "https://71gaj43a5l.execute-api.ap-south-1.amazonaws.com/prod/track";
+  (function logVisitor() {
+      const path = window.location.pathname;
+      fetch(`${TRACKING_API_URL}?path=${encodeURIComponent(path)}`)
+        .then(res => res.json())
+        .then(data => console.log("Visitor logged successfully", data))
+        .catch(err => console.error("Error logging visit", err));
+  })();
 
-// Log visitor
-fetch(LOG_ENDPOINT, { method: "GET" })
-  .then(res => console.log("Visit logged"))
-  .catch(err => console.error("Error logging visit", err));
-
-// Save visitor data on form submit
-document.getElementById("savevisitor").onclick = function () {
-  const inputData = {
-    name: $('#name').val(),
-    email: $('#email').val(),
-    subject: $('#subject').val(),
-    message: $('#message').val()
-  };
-
-  $.ajax({
-    url: API_ENDPOINT,
-    type: 'POST',
-    data: JSON.stringify(inputData),
-    contentType: 'application/json; charset=utf-8',
-    success: function (response) {
-      document.getElementById("visitorSaved").innerHTML = "Visitor data saved!";
-    },
-    error: function () {
-      alert("Error saving visitor data.");
-    }
-  });
-};
-
-// Fetch all visitor data
-document.getElementById("getvisitors").onclick = function () {
-  $.ajax({
-    url: API_ENDPOINT,
-    type: 'GET',
-    contentType: 'application/json; charset=utf-8',
-    success: function (response) {
-      $('#visitorTable tr').slice(1).remove(); // clear old rows
-      $.each(response, function (i, data) {
-        $("#visitorTable").append(`<tr>
-          <td>${data.name}</td>
-          <td>${data.email}</td>
-          <td>${data.subject}</td>
-          <td>${data.message}</td>
-        </tr>`);
+  // Contact Form Save (POST)
+  var API_ENDPOINT = "https://71gaj43a5l.execute-api.ap-south-1.amazonaws.com/prod";
+  document.getElementById("savevisitor").onclick = function(){
+      var inputData = {
+          "name": $('#name').val(),
+          "email": $('#email').val(),
+          "subject": $('#subject').val(),
+          "message": $('#message').val()
+      };
+      $.ajax({
+          url: API_ENDPOINT,
+          type: 'POST',
+          data:  JSON.stringify(inputData),
+          contentType: 'application/json; charset=utf-8',
+          success: function (response) {
+              document.getElementById("visitorSaved").innerHTML = "visitor Data Saved!";
+          },
+          error: function () {
+              alert("Error saving visitor data.");
+          }
       });
-    },
-    error: function () {
-      alert("Error retrieving visitor data.");
-    }
-  });
-};
+  }
+
+  // Get Visitors (GET)
+  document.getElementById("getvisitors").onclick = function(){  
+      $.ajax({
+          url: API_ENDPOINT,
+          type: 'GET',
+          contentType: 'application/json; charset=utf-8',
+          success: function (response) {
+              $('#visitorTable tr').slice(1).remove();
+              jQuery.each(response, function(i, data) {          
+                  $("#visitorTable").append("<tr> \
+                      <td>" + data['name'] + "</td> \
+                      <td>" + data['email'] + "</td> \
+                      <td>" + data['subject'] + "</td> \
+                      <td>" + data['message'] + "</td> \
+                      </tr>");
+              });
+          },
+          error: function () {
+              alert("Error retrieving visitor data.");
+          }
+      });
+  }
+</script>
